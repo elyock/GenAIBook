c9210727217543bd4d98b61ea9e3019abcfb7b4b
elyock <kevin.coyle@andalusia-energy.com>
2025-07-01 15:59:18 -0700

    Modified endpoint and key to support gpt-35-turbo deployment. Changed to "chat.completion" since gpt-35-turbo does not support legacy "completion". Other minor changes.

--- chapters/ch03/Listing-3.3-petsalon.py
+++ chapters/ch03/Listing-3.3-petsalon.py
@@ -1,24 +1,35 @@
 import os
+from dotenv import load_dotenv
 from openai import AzureOpenAI
 
+load_dotenv()
+
+aoai_endpoint = os.getenv("AOAI_GPT-35-TURBO_ENDPOINT")
+aoai_key = os.getenv("AOAI_GPT-35-TURBO_KEY")
+
+if aoai_endpoint is None or aoai_key is None:
+    raise ValueError("AOAI environment variables must be set.")
+
 client = AzureOpenAI(
-    azure_endpoint=os.getenv("AOAI_ENDPOINT"),
-    api_version="2024-05-01-preview",
-    api_key=os.getenv("AOAI_KEY"))
+    azure_endpoint=aoai_endpoint,
+    api_version="2024-12-01-preview",
+    api_key=aoai_key
+)
 
 GPT_MODEL = "gpt-35-turbo"
 
 prompt_startphrase = "Suggest three names for a new pet salon business. The generated name ideas should evoke positive emotions and the following key features: Professional, friendly, Personalized Service."
 
-response = client.completions.create(
+response = client.chat.completions.create(
     model=GPT_MODEL,
-    prompt=prompt_startphrase,
+    messages=[
+        {"role": "user", "content": prompt_startphrase}
+    ],
     temperature=0.7,
     max_tokens=100,
     #best_of=5,
     n=3,
     stop=None)
 
-responsetext = response.choices[0].text
-
-print("Prompt:" + prompt_startphrase + "\nResponse:" + responsetext)
+responsetext = response.choices[0].message.content
+print("Prompt:", prompt_startphrase, "\nResponse:", responsetext)
