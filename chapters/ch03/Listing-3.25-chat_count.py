8ed346267bc6e4dffe82e0992c08eeae47abf16d
elyock <kevin.coyle@andalusia-energy.com>
2025-07-02 14:20:19 -0700

    Get vars from .env.  Add type hints.

--- chapters/ch03/Listing-3.25-chat_count.py
+++ chapters/ch03/Listing-3.25-chat_count.py
@@ -1,11 +1,21 @@
 import os
 from openai import AzureOpenAI
 import tiktoken
+from dotenv import load_dotenv
+
+load_dotenv()
+
+azure_endpoint = os.getenv("AOAI_GPT-35-TURBO_ENDPOINT")
+api_key = os.getenv("AOAI_GPT-35-TURBO_KEY")
+
+if not azure_endpoint or not api_key:
+    raise ValueError("AOAI environment variables must be set.")
 
 client = AzureOpenAI(
-    azure_endpoint=os.getenv("AOAI_ENDPOINT"),
-    api_version="2024-05-01-preview",
-    api_key=os.getenv("AOAI_KEY"))
+    azure_endpoint=azure_endpoint,
+    api_version="2024-12-01-preview",
+    api_key=api_key)
+
 
 # This model name is what you chose when you deployed the model in Azure OpenAI
 GPT_MODEL = "gpt-35-turbo"
@@ -13,10 +23,10 @@ GPT_MODEL = "gpt-35-turbo"
 system_message = {"role": "system", "content": "You are a helpful assistant."}
 max_response_tokens = 250
 token_limit = 4096
-conversation = []
+conversation: list[dict[str, str]] = []
 conversation.append(system_message)
 
-def num_tokens_from_messages(messages):
+def num_tokens_from_messages(messages: list[dict[str, str]]) -> int:
     encoding= tiktoken.get_encoding("cl100k_base")
     num_tokens = 0
     for message in messages:
@@ -41,10 +51,12 @@ while True:
 
     response = client.chat.completions.create(
         model=GPT_MODEL,
-        messages=conversation,
+        messages=conversation, # type: ignore
         temperature=0.8,
         max_tokens=max_response_tokens)
 
-    conversation.append({"role": "assistant", "content": response.choices[0].message.content})
-    print("\n" + response.choices[0].message.content)
+    assistant_content = response.choices[0].message.content or ""
+    conversation.append({"role": "assistant", "content": assistant_content})
+    print("\n" + assistant_content)
     print("(Tokens used: " + str(response.usage.total_tokens)  + ")")
+    
