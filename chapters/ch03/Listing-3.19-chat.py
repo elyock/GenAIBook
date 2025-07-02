d99d3edf30a0d7585a19ac77d5a9193449b4ad4a
elyock <kevin.coyle@andalusia-energy.com>
2025-07-02 12:18:15 -0700

    Read vars from .env.

--- chapters/ch03/Listing-3.19-chat.py
+++ chapters/ch03/Listing-3.19-chat.py
@@ -1,10 +1,19 @@
 import os
 from openai import AzureOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+azure_endpoint = os.getenv("AOAI_GPT-35-TURBO_ENDPOINT")
+api_key = os.getenv("AOAI_GPT-35-TURBO_KEY")
+
+if not azure_endpoint or not api_key:
+    raise ValueError("AOAI environment variables must be set.")
 
 client = AzureOpenAI(
-    azure_endpoint=os.getenv("AOAI_ENDPOINT"),
-    api_version="2024-05-01-preview",
-    api_key=os.getenv("AOAI_KEY"))
+    azure_endpoint=azure_endpoint,
+    api_version="2024-12-01-preview",
+    api_key=api_key)
 
 # This model name is what you chose when you deployed the model in Azure OpenAI
 GPT_MODEL = "gpt-35-turbo"
